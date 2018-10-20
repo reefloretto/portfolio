9be6837a211fda353727d22f2aea5d5ec57ab3d0
Reef Loretto <reefloretto@gmail.com>
2018-10-19 23:05:26 -0400

    buffalo

--- src/App.js
+++ src/App.js
@@ -7,8 +7,31 @@ class App extends Component {
       <div className="app">
         <div className="header image">
           <div className="content">
-            <div>REEF LORETTO</div>
-            <div id="arabic">ريف لوريتو</div>
+            <div className="item">REEF LORETTO</div>
+            <div id="arabic" className="item">
+              ريف لوريتو
+            </div>
+          </div>
+        </div>
+        <div className="body">
+          <div className="row">
+            <div className="group-title">PROJECTS</div>
+          </div>
+          <div className="row project-title">
+            <a>blackbox</a>
+            <div className="year">2018</div>
+          </div>
+          <div className="row project-title">
+            <a>voices of boston</a>
+            <div className="year">2018</div>
+          </div>
+          <div className="row project-title">
+            <a>voices of aida</a>
+            <div className="year">2018</div>
+          </div>
+          <div className="row project-title">
+            <a>dmoz</a>
+            <div className="year">2018</div>
           </div>
         </div>
       </div>
